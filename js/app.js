032675f31c3cbeda36e8ddd7a6cdf496076b5e53
Andrii Spiian <andrey.spiyan@icloud.com>
2023-03-02 17:13:05 +0300

    create promt and alert examples

---
+++ js/app.js
@@ -0,0 +1,21 @@
+line = "--------";
+console.log(line);
+
+x = +prompt("Enter first number");
+y = +prompt("Enter second number");
+
+alert(`
+${x} + ${y} = ${x + y} 
+${x} - ${y} = ${x - y}
+${x} * ${y} = ${x * y}
+${x} / ${y} = ${x / y}
+`);
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+alert("Користувач ввiв " + x + " i " + y);
+
+line = "--------";
+console.log(line);
